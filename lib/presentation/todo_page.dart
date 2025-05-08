190761bce222c544cab51e1f18c4d0063a0be57b
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-08 22:19:47 +0700

    pesan kosong & membuat box hasil addtodo

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -96,6 +96,37 @@ class TodoPage extends StatelessWidget {
                   ],
                 ),
               ),
+              SizedBox(height: 16.0),
+              Expanded(
+                child: BlocBuilder<TodoBloc, TodoState>(
+                  builder: (context, state) {
+                    if (state is TodoLoading) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (state is TodoLoaded) {
+                      if (state.todos.isEmpty) {
+                        return Center(child: Text('Todo list is empty'));   // jika belum di add todonya maka akan menampilkan pesan tersebut
+                      }
+                      return ListView.builder(
+                        itemCount: state.todos.length,
+                        itemBuilder: (context, index) {
+                          final todo = state.todos[index];
+                          return Container(
+                            margin: EdgeInsets.only(bottom: 8.0),
+                            padding: EdgeInsets.all(16.0),
+                            decoration: BoxDecoration(
+                              color: Colors.blue[50],
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            
+                          );
+                        },
+                      );
+                    } else {
+                      return Center();
+                    }
+                  },
+                ),
+              ),
             ],
           ),)),
     );
