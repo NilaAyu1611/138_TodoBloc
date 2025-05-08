4edc8099e454b60a42f08cc3eae0f97d2aaadf1e
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-08 20:37:15 +0700

    menambahkan event untuk add todo dan tanda status

--- lib/bloc/todo_event.dart
+++ lib/bloc/todo_event.dart
@@ -9,3 +9,14 @@ final class TodoSelectDate extends TodoEvent {      //event untuk memilih tangga
   final DateTime date;
   TodoSelectDate({required this.date});
 }
+
+final class TodoEventAdd extends TodoEvent {
+  final String title;
+  final DateTime date;
+  TodoEventAdd({required this.title, required this.date});
+}
+
+final class TodoEventComplete extends TodoEvent {
+  final int index;
+  TodoEventComplete({required this.index});
+}
