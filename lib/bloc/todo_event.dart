d4ba4f0fc36bd0ad98e06a5d8ae0ed52e5ba7b17
NilaAyu1611 <nilaayucw@gmail.com>
2025-05-08 20:35:42 +0700

    menambahkan event untuk memuat todo & memilih tanggal

--- lib/bloc/todo_event.dart
+++ lib/bloc/todo_event.dart
@@ -2,3 +2,10 @@ part of 'todo_bloc.dart';
 
 @immutable
 sealed class TodoEvent {}
+
+final class TodoEventLoad extends TodoEvent {}
+
+final class TodoSelectDate extends TodoEvent {      //event untuk memilih tanggal
+  final DateTime date;
+  TodoSelectDate({required this.date});
+}
